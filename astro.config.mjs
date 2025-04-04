5af602635aa6a8945513e45c4b117fd1ff44962d
Gabriel Cunha <gabriel.cunha@ninjarmm.com>
2025-04-03 23:43:54 -0300

    actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
+// import { defineConfig } from 'astro/config';
+// import tailwind from '@astrojs/tailwind';
+
+// export default defineConfig({
+//   integrations: [tailwind()],
+// });
+
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://jugjoinville.github.io/jugjoinville.github.io/', // Update with your actual GitHub Pages URL
+  base: '/jugjoinville.github.io/',
 });
