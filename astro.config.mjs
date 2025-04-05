26479d3be70a77d6d0a2cc8898c540439addd96b
Gabriel Cunha <gabriel.cunha@ninjarmm.com>
2025-04-05 01:29:38 -0300

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,23 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import rehypePrettyCode from 'rehype-pretty-code';
+
+import sitemap from '@astrojs/sitemap';
+
+const prettyCodeOptions = {
+  theme: 'one-dark-pro',
+  keepBackground: false,
+};
 
 export default defineConfig({
-   integrations: [tailwind()],
-   site: 'https://jugjoinville.github.io',
+  site: 'https://jugjoinville.github.io/',
+  integrations: [tailwind(), mdx(), sitemap()],
+  markdown: {
+    rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
+    shikiConfig: {
+      theme: 'one-dark-pro',
+      wrap: true
+    }
+  }
 });
-
